5b7a446ef6149348484216588d3f0fb194d63c9b
automationtrainingcenter <atcsurya@gmail.com>
2020-01-25 18:19:58 +0530

    home and about pages

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, 'pages/home.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/home.html', context)
 
 
 def about(request):
-    # retrieve realtors and diplay
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('hire_date')[:3]
+    sop_realtor = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'sop_realtor': sop_realtor
+    }
+    return render(request, 'pages/about.html', context)
